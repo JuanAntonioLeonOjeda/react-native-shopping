cd6c250759bad6d6cf9bfda43e0a11feedc94648
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-12-15 19:00:54 +0000

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image } from 'react-native';
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
+  const handlePress = () => {
+    Alert.alert('Simple button pressed')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <StatusBar style="auto" /> */}
+      <View style={styles.container}>
+        <Text style={styles.title}>ShopTogether</Text>
+        <TouchableOpacity
+          onPress={handlePress}
+          style={[styles.Button, styles.newList]}
+        >
+          <Feather name="shopping-cart" size={100} color="black" />
+          <Text>Nueva Lista</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handlePress}
+          style={[styles.Button, styles.products]}
+        >
+          <MaterialCommunityIcons name="food-apple" size={100} color="black" />
+          <Text>Productos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handlePress}
+          style={[styles.Button, styles.history]}
+        >
+          <FontAwesome name="history" size={100} color="black" />
+          <Text>Historial</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100vh",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Button: {
+    flex: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "60%",
+    height: "25%",
+    borderWidth: 2,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  newList: {
+    backgroundColor: "lightblue",
+  },
+  products: {
+    backgroundColor: "lightgreen",
+  },
+  history: {
+    backgroundColor: "lightyellow",
+  },
+  title: {
+    fontSize: 40,
   },
 });
